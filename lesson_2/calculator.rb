924c03ab4fecc9a6e8c53e9817b3264967bb2e5f
d-a-grv <74752977+d-a-grv@users.noreply.github.com>
2021-05-26 12:18:59 -0400

    Modified calculator, RU and ENG language options

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,29 +1,66 @@
+require 'yaml'
+
+MESSAGES = { eng: YAML.load_file('calculator_messages.yml'),
+             ru: YAML.load_file('calculator_messages_ru.yml')
+}
+
 def prompt(message)
   puts "=> #{message}"
 end
 
 def valid_number?(num)
-  num.to_i > 0
+  valid_integer?(num) || valid_float?(num)
+end
+
+def valid_integer?(num)
+  num.to_i.to_s == num
+end
+
+def valid_float?(num)
+  num.to_f.to_s == num
 end
 
 def valid_operation?(op)
   %w(1 2 3 4).include?(op)
 end
 
-def operation_message(op_mess)
-  case op_mess
-  when '1'
-    'Adding'
-  when '2'
-    'Substracting'
-  when '3'
-    'Dividing'
-  when '4'
-    'Multiplying'
-  end
+def operation_message(op_mess, lang)
+  message = if lang == :eng
+              case op_mess
+              when '1'
+                'Adding'
+              when '2'
+                'Substracting'
+              when '3'
+                'Dividing'
+              when '4'
+                'Multiplying'
+              end
+            elsif lang == :ru
+              case op_mess
+              when '1'
+                'Складываю'
+              when '2'
+                'Отнимаю'
+              when '3'
+                'Делю'
+              when '4'
+                'Умножаю'
+              end
+            end
 end
 
-prompt("Welcome! Let's calculate some stuff! Enter your name:")
+prompt("Choose language: ENG or RU")
+
+language = gets.chomp
+
+if language.downcase.start_with?('ru')
+  language = :ru
+else
+  language = :eng
+end
+
+prompt(MESSAGES[language]['welcome'])
 
 name = ""
 
@@ -32,39 +69,31 @@ loop do
 
   break unless name.empty?
 
-  prompt("Please, enter a valid name:")
+  prompt(MESSAGES[language]['valid_name'])
 end
 
 loop do
   number1 = ''
   number2 = ''
 
-  prompt("#{name}, enter first number:")
+  prompt(name + MESSAGES[language]['first_number'])
 
   loop do
     number1 = gets.chomp
     break if valid_number?(number1)
-    prompt("Please, enter a valid number:")
+    prompt(MESSAGES[language]['valid_number'])
   end
 
-  prompt("Now, enter second number:")
+  prompt(MESSAGES[language]['second_number'])
 
   loop do
     number2 = gets.chomp
 
     break if valid_number?(number2)
-    prompt("Please, enter a valid number:")
+    prompt(MESSAGES[language]['valid_number'])
   end
 
-  operator_prompt = <<-MSG
-  What would you like to do with these numbers?
-  1) add
-  2) substract
-  3) divide
-  4) multiply
-  MSG
-
-  prompt(operator_prompt)
+  prompt(MESSAGES[language]['choice'])
 
   operation = ''
 
@@ -72,7 +101,7 @@ loop do
     operation = gets.chomp
 
     break if valid_operation?(operation)
-    prompt("Please, choose 1, 2, 3 or 4:")
+    prompt(MESSAGES[language]['valid_choice'])
   end
 
   result =  case operation.downcase
@@ -86,13 +115,13 @@ loop do
               number1.to_i * number2.to_i
             end
 
-  prompt("#{operation_message(operation)}...")
+  prompt("#{operation_message(operation, language)}...")
+  
+  prompt(MESSAGES[language]['result'] + "#{result}")
 
-  prompt("The result of the culculation is #{result}")
-
-  prompt("Would you like to do another calculation? (Y to do it again)")
+  prompt(MESSAGES[language]['repeat'])
 
   repeat = gets.chomp
 
-  break unless repeat.downcase.start_with?('y')
+  break unless repeat.downcase.start_with?('y') || repeat.downcase.start_with?('д')
 end

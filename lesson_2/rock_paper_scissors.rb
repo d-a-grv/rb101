eff6258072084b53658cfa3f8349f9de0aeb36fa
d-a-grv <74752977+d-a-grv@users.noreply.github.com>
2021-06-15 14:14:36 -0400

    Added lizard, Spock. Tracks score

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,43 +1,110 @@
-VALID_SCHOICES = ['rock', 'paper', 'scissors']
+require 'yaml'
+
+VALID_CHOICES = %w(rock paper scissors lizard spock)
+
+WIN_PAIRS = { r_sc: %w(rock scissors),
+              p_r: %w(paper rock),
+              sc_p: %w(scissors paper),
+              l_sp: %w(lizard spock),
+              l_p: %w(lizard paper),
+              r_l: %w(rock lizard),
+              sc_l: %w(scissors lizard),
+              sp_r: %w(spock rock),
+              sp_sc: %w(spock scissors),
+              p_sp: %w(paper spock) }
+
+ACTION_MESSAGE = YAML.load_file('rps_messages.yml')
 
 def prompt(message)
   puts "=> #{message}"
 end
 
-def display_result(player, computer)
-  if (player == 'rock' && computer == 'scissors') ||
-     (player == 'paper' && computer == 'rock') ||
-     (player == 'scissors' && computer == 'paper')
-    prompt('You won!')
-  elsif (player == 'rock' && computer == 'paper') ||
-       (player == 'paper' && computer == 'scissors') ||
-       (player == 'scissors' && computer == 'rock')
-      prompt('Computer won!')
+def who_wins?(player, computer)
+  player_pair = [player, computer]
+  computer_pair = [computer, player]
+
+  if WIN_PAIRS.value?(player_pair)
+    'player'
+  elsif WIN_PAIRS.value?(computer_pair)
+    'computer'
   else
-    prompt("It's a tie")
+    'tie'
+  end
+end
+
+def action(player, computer)
+  pair1 = [player, computer]
+  pair2 = [computer, player]
+
+  if WIN_PAIRS.value?(pair1)
+    ACTION_MESSAGE[WIN_PAIRS.key(pair1).to_s]
+  elsif WIN_PAIRS.value?(pair2)
+    ACTION_MESSAGE[WIN_PAIRS.key(pair2).to_s]
+  else
+    ACTION_MESSAGE['tie']
   end
 end
 
 loop do
-  prompt('Choose your weapon: rock, paper or scissors:')
-  choice = ''
+  score = { player: 0,
+            computer: 0 }
 
   loop do
-    choice = gets.chomp.downcase
+    prompt('Choose your weapon: rock, paper, scissors, lizard or Spock:')
+    player_choice = ''
+    loop do
+      player_choice = gets.chomp.downcase
 
-    break if VALID_SCHOICES.include?(choice)
-    prompt('Your choice is not valid. Try again:')
+      if player_choice == 's'
+        prompt('Did you mean to choose scissors or Spock?')
+        redo
+      end
+
+      VALID_CHOICES.each do |each_choice|
+        if each_choice.start_with?(choice)
+          player_choice = each_choice
+        end
+      end
+
+      break if VALID_CHOICES.include?(player_choice)
+      prompt('Your choice is not valid. Try again:')
+    end
+
+    computer_choice = VALID_CHOICES.sample
+
+    prompt("You chose #{player_choice}; computer chose #{computer_choice}")
+
+    result = ''
+    case who_wins?(player_choice, computer_choice)
+    when 'player'
+      result = 'You get a point!'
+      score[:player] += 1
+    when 'computer'
+      result = 'Computer gets a point!'
+      score[:computer] += 1
+    when 'tie'
+      "It's a tie."
+    end
+
+    message = action(player_choice, computer_choice) + result
+
+    prompt(message)
+
+    prompt('--------------------')
+    prompt('Score:')
+    prompt("User: #{score[:player]}")
+    prompt("Computer: #{score[:computer]}")
+    if score[:player] == 3
+      prompt("Thank you for playing! You won.")
+      break
+    elsif score[:computer] == 3
+      prompt("Thank you for playing! Computer won.")
+      break
+    end
   end
-
-  computer_choice = VALID_SCHOICES.sample
-
-  prompt("You chose #{choice}; computer chose #{computer_choice}")
-
-  display_result(choice, computer_choice)
-
-  prompt('Would you like to play again?')
+  prompt('Would you like to play again? (Y to play again)')
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt('Thank you. Good bye!')
+prompt('Good bye!')

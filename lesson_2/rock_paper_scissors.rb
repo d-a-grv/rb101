49381f5a801909382b81ac78b04bb4d77bf796f2
d-a-grv <74752977+d-a-grv@users.noreply.github.com>
2021-08-12 17:29:57 -0400

    Refined RPS

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,60 +1,129 @@
 require 'yaml'
 
-VALID_CHOICES = %w(rock paper scissors lizard spock)
+VALID_CHOICES = %w(rock paper scissors lizard Spock)
 
-WIN_PAIRS = { r_sc: %w(rock scissors),
-              p_r: %w(paper rock),
-              sc_p: %w(scissors paper),
-              l_sp: %w(lizard spock),
-              l_p: %w(lizard paper),
-              r_l: %w(rock lizard),
-              sc_l: %w(scissors lizard),
-              sp_r: %w(spock rock),
-              sp_sc: %w(spock scissors),
-              p_sp: %w(paper spock) }
-
-ACTION_MESSAGE = YAML.load_file('rps_messages.yml')
+WIN_PAIRS = {
+  'rock' => {
+    'scissors' => 'crushes',
+    'lizard' => 'crushes'
+  },
+  'paper' => {
+    'rock' => 'covers',
+    'Spock' => 'disproves'
+  },
+  'scissors' => {
+    'paper' => 'cut',
+    'lizard' => 'decapitate'
+  },
+  'lizard' => {
+    'Spock' => 'poisons',
+    'paper' => 'eats'
+  },
+  'Spock' => {
+    'rock' => 'vaporizes',
+    'scissors' => 'smashes'
+  }
+}
 
 WIN_SCORE = 3
 
 def clear_screen
-  system('clear')
+  system 'clear'
 end
 
 def prompt(message)
   puts "=> #{message}"
 end
 
-def who_wins?(player, computer)
-  player_pair = [player, computer]
-  computer_pair = [computer, player]
+def div
+  puts ''
+end
 
-  if WIN_PAIRS.value?(player_pair)
+def enter_to_continue
+  div
+  prompt('Press ENTER to continue:')
+  gets.chomp.downcase
+end
+
+def display_welcome
+  clear_screen
+  prompt('Welcome to Rock, Paper, Scissors, Lizard, Spock')
+  prompt("To win, you have to get #{WIN_SCORE} points. Good luck!")
+  enter_to_continue
+end
+
+def clarify
+  prompt('Did you mean to choose scissors or Spock?')
+  gets.chomp.downcase
+end
+
+def get_player_choice
+  prompt('Choose your weapon: rock, paper, scissors, lizard or Spock:')
+  player_choice = gets.chomp.downcase
+
+  player_choice = clarify(player_choice) if player_choice == 's'
+
+  VALID_CHOICES.each do |each_choice|
+    if each_choice.downcase.start_with?(player_choice)
+      player_choice = each_choice
+    end
+  end
+  player_choice
+end
+
+def display_score(scr)
+  prompt('--------------------')
+  prompt('Score:')
+  prompt("User: #{scr[:player]}")
+  prompt("Computer: #{scr[:computer]}")
+end
+
+def determine_round_winner(player, computer)
+  if WIN_PAIRS[player].key?(computer)
     'player'
-  elsif WIN_PAIRS.value?(computer_pair)
+  elsif WIN_PAIRS[computer].key?(player)
     'computer'
   else
     'tie'
   end
 end
 
-def action(player, computer)
-  pair1 = [player, computer]
-  pair2 = [computer, player]
+def someone_won?(scr)
+  scr[:player] == WIN_SCORE || scr[:computer] == WIN_SCORE
+end
 
-  if WIN_PAIRS.value?(pair1)
-    ACTION_MESSAGE[WIN_PAIRS.key(pair1).to_s]
-  elsif WIN_PAIRS.value?(pair2)
-    ACTION_MESSAGE[WIN_PAIRS.key(pair2).to_s]
-  else
-    ACTION_MESSAGE['tie']
+def determine_game_winner(scr)
+  if scr[:player] == WIN_SCORE
+    'player'
+  elsif scr[:computer] == WIN_SCORE
+    'computer'
   end
 end
 
-clear_screen
-prompt('Welcome to Rock, Paper, Scissors, Lizard, Spock')
-prompt("To win, you have to get #{WIN_SCORE} points. Good luck! (Press ENTER to continue)")
-continue_input = gets.chomp
+def display_win_message(scr)
+  div
+  case determine_game_winner(scr)
+  when 'player'
+    prompt('Thank you for playing. You won!')
+  when 'computer'
+    prompt('Thank you for playing. Computer won!')
+  end
+end
+
+def action(player, computer)
+  player = player.capitalize if player == 'spock'
+  computer = computer.capitalize if computer == 'spock'
+  case determine_round_winner(player, computer)
+  when 'player'
+    "#{player.capitalize} #{WIN_PAIRS[player][computer]} #{computer}."
+  when 'computer'
+    "#{computer.capitalize} #{WIN_PAIRS[computer][player]} #{player}."
+  when 'tie'
+    'Looks like you both chose the same weapon.'
+  end
+end
+
+display_welcome
 
 loop do
   score = { player: 0,
@@ -63,22 +132,9 @@ loop do
   loop do
     clear_screen
 
-    prompt('Choose your weapon: rock, paper, scissors, lizard or Spock:')
     player_choice = ''
     loop do
-      player_choice = gets.chomp.downcase
-
-      if player_choice == 's'
-        prompt('Did you mean to choose scissors or Spock?')
-        redo
-      end
-
-      VALID_CHOICES.each do |each_choice|
-        if each_choice.start_with?(player_choice)
-          player_choice = each_choice
-        end
-      end
-
+      player_choice = get_player_choice
       break if VALID_CHOICES.include?(player_choice)
       prompt('Your choice is not valid. Try again:')
     end
@@ -87,38 +143,31 @@ loop do
 
     prompt("You chose #{player_choice}; computer chose #{computer_choice}")
 
-    result = ''
-    case who_wins?(player_choice, computer_choice)
-    when 'player'
-      result = 'You get a point!'
-      score[:player] += 1
-    when 'computer'
-      result = 'Computer gets a point!'
-      score[:computer] += 1
-    when 'tie'
-      result = "It's a tie."
-    end
+    result = case determine_round_winner(player_choice, computer_choice)
+             when 'player'
+               score[:player] += 1
+               'You get a point!'
+             when 'computer'
+               score[:computer] += 1
+               'Computer gets a point!'
+             when 'tie'
+               "It's a tie."
+             end
 
-    message = action(player_choice, computer_choice) + result
+    message = action(player_choice, computer_choice) + ' ' + result
 
     prompt(message)
 
-    prompt('--------------------')
-    prompt('Score:')
-    prompt("User: #{score[:player]}")
-    prompt("Computer: #{score[:computer]}")
-    if score[:player] == WIN_SCORE
-      prompt("Thank you for playing! You won.")
-      break
-    elsif score[:computer] == WIN_SCORE
-      prompt("Thank you for playing! Computer won.")
-      break
-    end
+    display_score(score)
 
-    prompt('Ready for the next round? (Press ENTER to continue)')
-    continue_input = gets.chomp
+    break if someone_won?(score)
+
+    prompt('Ready for the next round?')
+    continue_input = enter_to_continue
     break if continue_input.downcase.start_with?('stop')
   end
+  display_win_message(score)
+
   prompt('Would you like to play again? (YES to play again)')
   again = gets.chomp
   break unless again.downcase.start_with?('yes')

4c1fb1eeede44aa88bdbb0e71198515d08bc6193
d-a-grv <74752977+d-a-grv@users.noreply.github.com>
2021-10-03 14:24:13 -0400

    Anagrams modified

--- small_problems/easy9/anagrams.rb
+++ small_problems/easy9/anagrams.rb
@@ -1,28 +1,9 @@
-=begin
-  P: given an array of strings, return an array/arrays containing strings that have the same letters
-
-  D: arrays
-
-  A:
-
--set (anagrams) to empty arr
-- start a loop that continues for as many times as many element there are in the array
-  -set (counter) to 0
-  -iterate through given array
-    -skip the element if element at index (counter) is the same
-    -check if the current word has the same characters as the word at index (counter)
-      -if it does, append it to the (anagrams)
--return (anagrams)
-
-=end
-
 def get_anagrams(arr)
   anagrams = [[]]
 
   arr.length.times do |counter|
     anagrams << [] unless anagrams.include?([])
     arr.each do |word|
-      
       anagrams[counter] << word if word.delete(arr[counter]).empty?
     end
   end
